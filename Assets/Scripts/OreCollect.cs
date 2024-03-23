1b571a3546c1a347be21215b23a1e964157dfec3
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2024-03-23 16:37:54 +0200

    HUD graphics improvement
    Updated the HUD graphics, fixed bug with orecollect script, now only destroys the grabbed ore.

--- Assets/Scripts/OreCollect.cs
+++ Assets/Scripts/OreCollect.cs
@@ -1,30 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 
 public class OreCollect : MonoBehaviour
 {
     public static int oreCount = 0;
-    public InputActionReference action;
-    
-   
-    private void OnEnable()
-    {
-        action.action.Enable();
-        action.action.performed += OnActionPerformed;
-    }
-
-    private void OnDisable()
-    {
-        action.action.Disable();
-        action.action.performed -= OnActionPerformed;
-    }
-    private void OnActionPerformed(InputAction.CallbackContext context)
-    {
-        {
+  
+ 
+    public void oreCollect(){
             Destroy(gameObject);
             oreCount++;
-        }
     }
 }
