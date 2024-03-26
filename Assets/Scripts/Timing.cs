33c66c44cc919020cc9dfa1f87a34b9ee8c4af1c
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2024-03-26 12:49:39 +0200

    Timing updated
    Updated the flashing in timing script

--- Assets/Scripts/Timing.cs
+++ Assets/Scripts/Timing.cs
@@ -13,31 +13,53 @@ public class Timing : MonoBehaviour
     public float flashDelayMax = 12.0f;
     public float lightOnTime = 2.0f;
     public bool lightEvent = false;
+    public int flashValue;
+    public float redFlashMin = 10f;
+    public float redFlashMax = 20f;
+    public float redFlashRange;
+    public float flashRange;
+    public bool isRed = false;
 
     void Start()
     {
+        redFlashRange = Mathf.Round(Random.Range(redFlashMin, redFlashMax));
         StartCoroutine(FlashRoutine());
- 
+        flashing.text = "O";
+        
     }
     
     IEnumerator FlashRoutine()
     {
-        while (true)
+        while (!isRed)
         {
-            
+            StartCoroutine(LightOnEvent());
+            flashRange++;
             float flashDelay = Random.Range(flashDelayMin, flashDelayMax);
             yield return new WaitForSeconds(flashDelay);
-            flashing.color = Color.red;
-            lightEvent = true;
-            StartCoroutine(LightOnEvent());
+            flashing.text = "O";
+            
+
+            
+            if(flashRange == redFlashRange){
+                StopCoroutine(FlashRoutine());
+                isRed = true;
+                flashing.text = "O";
+                lightEvent = true;
+                redFlashRange = Mathf.Round(Random.Range(redFlashMin, redFlashMax));
+                flashing.color = Color.red;
+                flashRange = 0f;
+                flashValue = 2;
+            }
+            
             yield return null;
         }
     }
-     IEnumerator LightOnEvent()
+
+    IEnumerator LightOnEvent()
     {
         yield return new WaitForSeconds(lightOnTime);
-        flashing.color = Color.white;
-        lightEvent = false;
+        flashing.text = "";
+        flashValue = 1;
     }
 
       private void OnEnable()
@@ -57,6 +79,12 @@ public class Timing : MonoBehaviour
         {
             StopCoroutine(LightOnEvent());
             lightEvent = false;
+            flashing.text = "";
+            flashing.color = Color.white;
+            isRed = false;
+            StartCoroutine(FlashRoutine());
+        }
+        else {
             flashing.color = Color.white;
         }
     }
