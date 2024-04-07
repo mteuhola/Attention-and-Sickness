1fe8f0085658c0750a6b2b64227bfa148618f573
Markus Teuhola <mteuhola19@student.oulu.fi>
2024-04-07 16:43:23 +0300

    Timer added

---
+++ Assets/Scripts/SceneTimer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Time elapsed in the current scene
+    private float currentTime = 0f;
+    private bool timerRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Reset timer when a new scene is loaded
+        currentTime = 0f;
+        // Start the timer
+        timerRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerRunning)
+        {
+            currentTime += Time.deltaTime;
+            Debug.Log(currentTime);
+        }
+    }
+}
